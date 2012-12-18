478015ad842abc0d5402dd8e32f12ada2bd4224a
Sergey Muto <semsergey@gmail.com>
2012-12-19 00:46:49 +0900

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eugine::Application.config.secret_token = '2103d4480352526ece97c28078857746e439be52b0588c29d26b3f4167f6d403c670c1cd508de6493a0aef2a6d00236092b85f45329cba6e5dbe21da9d9f077d'
